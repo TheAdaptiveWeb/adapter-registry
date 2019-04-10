b4460fbd883dc2410dc4f608a7e5f4a9bc9e440b
Adam Hirst <adam@ahirst.com>
2019-04-10 13:46:21 +0100

    Added symbol-translator

--- adapters/index.js
+++ adapters/index.js
@@ -1,5 +1,6 @@
 module.exports = [
     require('./contrast-adapter/dist'),
     require('./video-audio-description/dist'),
-    require('./alt-tag-generator/dist')
+    require('./alt-tag-generator/dist'),
+    require('./symbol-translator/dist')
 ]
